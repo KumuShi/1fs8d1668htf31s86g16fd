3a5326567e74c7dc694a7178b7ec8ab3b82c92dc
KumuShi <61600114+KumuShi@users.noreply.github.com>
2020-10-04 18:53:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -34,7 +34,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`Waiting for commands ! 🌸`, {type: 4});
+  client.user.setActivity(`master's commands 🌸`, {type: "SREAMING"});
 });
 
 client.on("guildMemberAdd", (user)=>{
